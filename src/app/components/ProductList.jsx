5d74b3c2c0ff657cfe916cb449d0ea229e9f7ac3
Tristan Zybala <tzybala@usf.edu>
2025-11-02 22:34:10 -0500

    Product List created

--- src/app/components/ProductList.jsx
+++ src/app/components/ProductList.jsx
@@ -1,1 +1,19 @@
 'use client';
+
+import React from 'react';
+import ProductCard from './ProductCard';
+
+function ProductList({ products, onInCart }) {
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6'>
+            {products.map((product) => (
+                <ProductCard
+                    key={product.id}
+                    product={product}
+                    onInCart={onInCart}/>
+            ))}
+        </div>
+    );
+}
+
+export default ProductList;
