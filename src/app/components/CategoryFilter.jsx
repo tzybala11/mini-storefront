cb0673af1139b4be01a3714c483e78f42d25e1ce
Tristan Zybala <tzybala@usf.edu>
2025-11-02 23:00:19 -0500

    Category Filter with simple Inline CSS

--- src/app/components/CategoryFilter.jsx
+++ src/app/components/CategoryFilter.jsx
@@ -1,1 +1,28 @@
 'use client';
+
+import React from 'react';
+
+function CategoryFilter({ categoryFilter, setCategoryFilter, categories }) {
+    const handleChange = (event) => {
+        setCategoryFilter(event.target.value);
+    };
+
+    return (
+        <div className='bg-white border'>
+            <label htmlFor='category' className='block text-sm font-medium'>
+                Filter by Category:
+            </label>
+            <select
+                id='category'
+                name='category'
+                value={categoryFilter}
+                onChange={handleChange}
+                className='mt-1 block w-full pl-3 pr-10 py-2 text-base'>
+                <option value=''>All Categories</option>
+                {categories.map((category) => (
+                    <option key={category} value={category}>{category}</option>
+                ))}
+                </select>
+        </div>
+    );
+}
